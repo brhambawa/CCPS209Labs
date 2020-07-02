8e58883ba48c645d1912540a6457f3795b5d60b5
Ilkka Kokkarinen <ilkka.kokkarinen@gmail.com>
2020-07-02 16:17:29 -0400

    Add files via upload

--- DistanceTestThree.java
+++ DistanceTestThree.java
@@ -12,6 +12,7 @@ public class DistanceTestThree {
 
     private static final int SEED = 12345;
     private static final int PREC = 30;
+    private static final Distance ZERO = new Distance(0, 1);
     
     @Test public void testApproximate() {
         Random rng = new Random(SEED);
@@ -20,7 +21,7 @@ public class DistanceTestThree {
         MathContext mc2 = new MathContext(PREC + 2, RoundingMode.HALF_UP);
         BigDecimal epsilon = new BigDecimal(1).scaleByPowerOfTen(PREC);
         for(int i = 1; i < 100; i++) {
-            Distance d = Distance.ZERO;
+            Distance d = ZERO;
             BigDecimal prev = new BigDecimal(0, mc2);
             for(int j = 0; j*j < i; j++) {
                 int whole = rng.nextInt(i + 3) + 1;
@@ -41,6 +42,32 @@ public class DistanceTestThree {
             }
         }
     }  
+    
+    @Test public void massTestCollections() {
+        Random rng = new Random(SEED);
+        HashSet<Distance> hs = new HashSet<>();
+        TreeSet<Distance> ts = new TreeSet<>();
+        for(int i = 0; i < 1000; i++) {
+            Distance d = new Distance(0, 1);
+            for(int k = 0; k <= i % 20; k++) {
+                int whole = rng.nextInt(i + 3);
+                if(rng.nextBoolean()) { whole = -whole; }
+                int base = rng.nextInt(10 * (i + 3)) + 1;
+                d = d.add(new Distance(whole, base));
+            }            
+            // The distance object is either member or nonmember of both sets.
+            assertEquals(hs.contains(d), ts.contains(d));
+            // Add it in both.
+            hs.add(d);
+            ts.add(d);
+            // Both must be the same size.
+            assertEquals(hs.size(), ts.size());
+        }
+        // Both sets must now contain the same objects.
+        for(Distance d: hs) { assertTrue(ts.contains(d)); }
+        for(Distance d: ts) { assertTrue(hs.contains(d)); }
+        
+    }
     
     @Test public void testCompareTo() {
         MathContext mc = new MathContext(10);
